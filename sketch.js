62eb189bbc898768b7ed04f2450e31b215323759
Kirtan101 <98946938+Kirtan101@users.noreply.github.com>
2022-02-03 17:26:29 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+ var ball; 
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,30,30);
+  ball.shapeColor= "yellow";
+}
+
+function draw() 
+{
+  background(0);
+  drawSprites();
+
+}
+
+
+
+
